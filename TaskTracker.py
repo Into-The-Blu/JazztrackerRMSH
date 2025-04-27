e538e1cdc0cb1190fd4b3b5e12da17d6c0de9e01
Into-The=Blu <jasmine.r.timson@gmail.com>
2025-04-27 04:55:15 +0100

    Added base class and main function

--- TaskTracker.py
+++ TaskTracker.py
@@ -1,7 +1,42 @@
-#
-#
-#
-#
+import re
+
+taskList = [] #test, temporary
+
+cmdList = "^add|^delete|^update|^mark|^list"
+
+class task:
+    def __init__(self, body, id, status, createdAt, updatedAt):
+        self.body = body
+        self.id = id
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+
+def inputMonitor():
+    x = re.search(cmdList, input())
+    if x == true:
+        match x:
+            case "add":
+                pass
+            case "delete":
+                pass
+            case "update":
+                pass
+            case "mark":
+                pass
+            case "list":
+                pass
+    else: 
+        y = input()
+        y = y.split(" ")
+        del y[1]
+        y = str(y)
+        y = y.strip("['']")
+        print(f"[ERROR]: command '{y}' not recognised")
+
+
+    
+
 
 
 
