37c137bd4900f96a2ffab0099a623f84c44159fe
Into-The=Blu <jasmine.r.timson@gmail.com>
2025-04-29 21:29:26 +0100

    Moved most funcs into unified task manager, redid inputMonitor(), redid most of the program with help from the gf

--- Jazztracker.py
+++ Jazztracker.py
@@ -5,90 +5,74 @@
 import re
 import datetime
 import math
+from dataclasses import dataclass
 
 #Todo:
 #-rework whole thing for that way you thought of
 #-add json handling
 
 #test list:
-#add case works, function half works
-#inputMonitor function - works so far
-#everything else - not tested
 
 taskList = [] #temporary, in place of json
-#cmdList = "^add|^delete|^update|^mark|^list"
-class task:
-    def __init__(self, id, body, status, createdAt, updatedAt):
-        self.id = id
-        self.body = body
-        self.status = status
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-statusFilter = None
+@dataclass
+class Task:
+    taskId_objAttr: int
+    body: str
+    status: str
+    createdAt: str
+    updatedAt: str
+
 def help():
     print(f"\nCommands:\n============================================================================\nadd <task>...................adds a task with the default status \"todo\"\ndelete <taskid>..............deletes the task associated with the given id\nupdate <taskid> <task>.......overwrites the task associated with the given id\nmark-in-progress <taskid>....marks the task at a given id as in-progress\nmark-done <taskid>...........marks the task at a given id as done\nlist.........................lists all tasks\nlist todo....................lists all tasks tagged as todo\nlist in-progress.............lists all tasks tagged as in-progress\nlist done....................lists all tasks tagged as done\n============================================================================\n")
 
-def add():
-    taskTemp = task(0, None, "todo", datetime.datetime.now() , 0) #refine the dt
-    taskTemp.id = id
-    taskTemp.body = userInput[1]
-    taskList.insert(id, taskTemp)
-    print(taskList)
-    print(taskList[0].body)
-    if len(taskList) > 1:
-        print(taskList[1].body)
-    print(f"\nTask succesfully created! ID: {id}\n")
-    del taskTemp
-    
-def delete():
-    taskList[id] = None
-    print(f"\nTask ID: {userInput[1]} succesfully deleted!\n")
-
-def printList():
-    for i in range(len(taskList)):
-        if (taskList[i] == True) and (statusFilter == taskList[i].status or False):
-            print(f"ID: {i + 1} | Date created/updated: {taskList[i].createdAt} | {taskList[i].body}")
-        else:
-            continue
-
-def idHandler():
-    pass
-
-def inputMonitor():
-    global userInput
-    userInput = input().split(" ")
-    match userInput[0]:
+def taskManager(commandCalled, param1, param2):
+    match commandCalled:
         case "add":
-            idHandler()
-            add()
+            taskTemp = Task(taskId_objAttr=taskId, body=str, status="todo", createdAt=datetime.datetime.now() , updatedAt=str) #refine the dt
+            taskTemp.taskId_objAttr = taskId
+            taskTemp.body = param1
+            taskList.insert(taskId, taskTemp)
+            print(f"\nTask succesfully created! ID: {taskId}\n")
+            return taskList
         case "delete":
-            delete()
+            taskList.taskId_objAttr.remove(param1)
+            return taskList
         case "update":
-            taskList[id].updatedAt = datetime.datetime.now() #refine this
-        case "mark-in-progress":
-            id = userInput[1] - 1
-            taskList[id].status = "in progress"
-        case "mark-done":
-            id = userInput[1] - 1
-            taskList[id - 1].status = "done"
+            taskList[taskList.taskId_objAttr.index(param1)].body = param2
+            return taskList
         case "list":
-            if len(userInput) > 1:
-                match userInput[1]:
-                    case "done":
-                        printList()
-                    case "todo":
-                        statusFilter = userInput[1]
-                        printList()
-                    case "in-progress":
-                        statusFilter = userInput[1]
-                        printList()
+            if param1 == True:
+                printList(param1, True)
             else:
-                printList()
+                printList(None, False)
+        case "mark-in-progress":
+            taskList[taskList.taskId_objAttr.index(param1)].status = "in progress"
+            return taskList
+        case "mark-done":
+            taskList[taskList.taskId_objAttr.index(param1)].status = "done"
+            return taskList
         case "help":
             help()
         case _:
-            print(f"[ERROR]: command '{userInput[0]}' not recognised")
-    userInput = []
+            print(f"[ERROR]: command '{commandCalled}' not recognised")
+
+def printList(statusFilter, filterActive):
+    for i in enumerate(taskList):
+        if taskList[i].status == statusFilter or filterActive == False:
+            print(f"ID: {taskList[i].taskId_objAttr} | Date created/updated: {taskList[i].createdAt} | {taskList[i].body}")
+
+def idHandler():
+    return taskId
+
+def inputMonitor():
+    userInput = input().split(" ")
+    try:
+        taskManager(userInput[0], userInput[1], userInput[2])
+    except:
+        try: 
+            taskManager(userInput[0], userInput[1], False)
+        except: 
+            taskManager(userInput[0], False, False)
     inputMonitor()
 
 print(f"\nWelcome to Jazztracker v0.5! Get started by adding a task, or use the \"help\" command if you're stuck.\n")     
@@ -101,7 +85,6 @@ inputMonitor()
 #mark-in-progress <taskid>
 #mark-done <taskid>
 #list
-#list todo
 #list in-progress
 #list done
 
